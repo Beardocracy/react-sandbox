88b2926d6572ef0e20dcb1a28c297348114ab1ea
Beardocracy <travisjbearden@gmail.com>
2021-03-16 15:50:09 -0500

    Added additional useEffect to avoid extra unnecessary API calls

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,9 +3,19 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('Ghosts');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
-    // Not allowed to be directly async/await.
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [term]);
+    
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -14,7 +24,29 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm
+                }
+            });
+            setResults(data.query.search);
+        };
+        if (debouncedTerm) {
+            search();
+        }
+
+    }, [debouncedTerm]);
+
+    // Not allowed to be directly async/await.
+    /* This whole block was changed when I added the debouncedTerm state.
+        It makes it so we don't have extra API calls, from monitoring results.length
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debouncedTerm
                 }
             });
             setResults(data.query.search);
@@ -44,7 +76,7 @@ const Search = () => {
     // []: Run at initial render only.
     // [data]: Run after every rerender IF data has changed since last rerender.
     //         Can have multiple. Only one must change for it to be executed.
-
+*/
     
     
     const renderedResults = results.map((result) => {
