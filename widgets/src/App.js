ab973ffd09f2b4c71e3f7f0373f4dfd24d20c82a
Beardocracy <travisjbearden@gmail.com>
2021-03-16 17:33:31 -0500

    Added dropdown widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import Accordion from './components/Accordion';
-import Search from './components/Search';
+//import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 /*
 const items = [
     {
@@ -17,10 +18,36 @@ const items = [
     }
 ];
 */
+
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Purple',
+        value: 'purple'
+    },
+    {
+        label: 'A shade of Blue',
+        value: 'blue'
+    }
+];
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
+
     return (
         <div>
-            <Search />
+            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+            {showDropdown ? 
+                <Dropdown
+                    selected={selected}
+                    options={options}
+                    onSelectedChange={setSelected}
+                /> : null
+            }
         </div>
     );
 }
---
+++ widgets/src/components/Dropdown.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const Dropdown = ({options, selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    // Sets up a click event handler to close the dropdown if we click outside the referenced div.
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            // Don't close if we click on something inside the referenced div.
+            // Let those divs' logic handle it.
+            // Events always bubble up.
+            if (ref.current && ref.current.contains(event.target)) {
+                return;
+            }
+        
+            setOpen(false);
+        };
+        
+        document.body.addEventListener('click', onBodyClick);
+           
+        // Is called when dropdown is removed.
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
+    }, []);
+
+    const renderedOptions = options.map((option) => {
+        
+        if (option.value === selected.value) {
+            return null;
+        }
+        
+        return (
+            <div 
+                key={option.value} 
+                className="item" 
+                onClick={() => {
+                    //console.log('Item Clicked');
+                    onSelectedChange(option);
+                }}
+            >
+                {option.label}
+            </div>
+        );
+    });    
+
+    console.log(ref.current);
+
+    return (
+        <div ref={ref} className="ui form">
+            <div className="field">
+                <label className="label">Select a color</label>
+                <div 
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                    onClick={() => {
+                        //console.log('Dropdown Clicked');
+                        setOpen(!open);
+                    }}
+                >
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dropdown;
